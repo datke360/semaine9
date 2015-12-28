1076e79000c813e2b65bd19b7aef4b27f77105f5
datke360 <kevin_datiche@hotmail.fr>
2015-12-28 11:45:52 +0000

    semaine 9

---
+++ Ex2/script.js
@@ -0,0 +1,29 @@
+$liste = document.getElementById("liste");
+$texte = document.getElementById("texte");
+$ajouter = document.getElementById("ajouter");
+$annuler = document.getElementById("annuler");
+
+
+tableau = ["Bob", "Julien", "Roger"];
+
+function afficherListe() {
+    elements = "";
+    for (i = 0; i < tableau.length; i++) {
+        elements += "<li>" + tableau[i] + "</li>";
+    }
+    $liste.innerHTML = elements;
+}
+
+function ajouter() {
+    tableau.push($texte.value);
+    afficherListe();
+}
+
+function annuler() {
+    tableau.pop($texte.value);
+    afficherListe();
+}
+
+$ajouter.onclick = ajouter;
+$annuler.onclick = annuler;
+afficherListe();
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # semaine9
+
+liens badges codecademy : https://www.codecademy.com/fr/users/datiche36/achievements
+
+Ex1 : 
+
+Ex2 :
+
+Ex3 :
